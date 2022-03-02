8d0d2829ff08258074dddd5783ecfbe9ee647bed
pham phuoc <phhphc@gmail.com>
2022-03-02 14:44:56 +0700

    go

--- back-end/main/settings.py
+++ back-end/main/settings.py
@@ -20,26 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6nl#*7+#dq8wmh^(@f1%)vr09e#k9))nf9*s2v&zynh40j9t(4'
+SECRET_KEY = 'django-insecure-&x^5&gi2(vzmd&6hre3q9hqa#)vh8##*i8c(nqgnhb0jqulvi2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-# config for CORS issues
-CORS_ALLOWED_ORIGINS = [
-    # front-end URL here
-    "http://127.0.0.1:3000",
-    "http://localhost:3000",
-    "https://phhphc.github.io",
-]
-CORS_ALLOW_CREDENTIALS= True
-CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SAMESITE = 'None'
-SESSION_COOKIE_SAMESITE = 'None'
 
 
 # Application definition
@@ -50,20 +36,16 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'rest_framework',
-    "corsheaders",
-
     'django.contrib.staticfiles',
+    'rest_framework',
 
     'user_api',
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
-    'django.middleware.common.CommonMiddleware',
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -133,8 +115,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = "static/"
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
